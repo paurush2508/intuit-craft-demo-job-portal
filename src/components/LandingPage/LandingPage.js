00d682ee5f1417d03182f27ddd1a11c8f69033ef
p0p038b <Paurush.Paurush@walmart.com>
2024-02-18 19:41:52 +0530

    added landing page comp

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TopNavBar from "./TopNavBar";
+import Header from "./Header";
+import Projects from "./Projects";
+
+function LandingPage() {
+  return (
+    <>
+      <TopNavBar />
+      <Header />
+      <Projects />
+    </>
+  );
+}
+
+export default LandingPage;
