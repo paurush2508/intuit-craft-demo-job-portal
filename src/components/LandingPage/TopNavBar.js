f6abf5d0910bff509c3519180cf1d3167f990e39
p0p038b <Paurush.Paurush@walmart.com>
2024-02-18 19:43:28 +0530

    added nav bar

---
+++ src/components/LandingPage/TopNavBar.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-scroll";
+import LogoIcon from "../../assets/svg/LogoIcon";
+
+export default function TopNavbar() {
+  const [y, setY] = useState(window.scrollY);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => setY(window.scrollY));
+    return () => {
+      window.removeEventListener("scroll", () => setY(window.scrollY));
+    };
+  }, [y]);
+
+  return (
+    <>
+      <Wrapper
+        className="flexCenter animate whiteBg"
+        style={y > 100 ? { height: "60px" } : { height: "80px" }}
+      >
+        <NavInner className="container flexSpaceCenter">
+          <Link className="pointer flexNullCenter" to="home" smooth={true}>
+            <LogoIcon />
+            <h1 style={{ marginLeft: "15px" }} className="font20 extraBold">
+              Intuit Job Portal
+            </h1>
+          </Link>
+          <UlWrapper className="flexNullCenter">
+            <li className="semiBold font15 pointer">
+              <Link
+                activeClass="active"
+                style={{ padding: "10px 15px" }}
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={-80}
+              >
+                Home
+              </Link>
+            </li>
+
+            <li className="semiBold font15 pointer">
+              <Link
+                activeClass="active"
+                style={{ padding: "10px 15px" }}
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={-80}
+              >
+                Trending Companies
+              </Link>
+            </li>
+            <li className="semiBold font15 pointer">
+              <Link
+                style={{ padding: "10px 15px" }}
+                spy={true}
+                smooth={true}
+                offset={-80}
+              >
+                Jobs
+              </Link>
+            </li>
+            <li className="semiBold font15 pointer">
+              <Link
+                style={{ padding: "10px 15px" }}
+                spy={true}
+                smooth={true}
+                offset={-80}
+              >
+                Services
+              </Link>
+            </li>
+          </UlWrapper>
+          <UlWrapperRight className="flexNullCenter">
+            <li className="semiBold font15 pointer">
+              <a href="/" style={{ padding: "10px 30px 10px 0" }}>
+                Log in
+              </a>
+            </li>
+            <li className="semiBold font15 pointer flexCenter">
+              <a
+                href="/"
+                className="radius8 lightBg"
+                style={{
+                  padding: "10px 15px",
+                  background: "rgb(118, 32, 255)",
+                  color: "#fff",
+                }}
+              >
+                Sign up
+              </a>
+            </li>
+          </UlWrapperRight>
+        </NavInner>
+      </Wrapper>
+    </>
+  );
+}
+
+const Wrapper = styled.nav`
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+`;
+const NavInner = styled.div`
+  position: relative;
+  height: 100%;
+`;
+const UlWrapper = styled.ul`
+  display: flex;
+  @media (max-width: 760px) {
+    display: none;
+  }
+`;
+const UlWrapperRight = styled.ul`
+  @media (max-width: 760px) {
+    display: none;
+  }
+`;
