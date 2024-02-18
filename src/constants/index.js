b116628e72cc2b5d952fb5289f59f6918168c4f5
p0p038b <Paurush.Paurush@walmart.com>
2024-02-18 23:20:14 +0530

    constant for navs

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,32 +1,47 @@
 export const companies = [
-    {
-      img: "https://photos.wellfound.com/startups/i/33986-687620e87450ee4654c88a0417d81358-medium_jpg.jpg?buster=1589601889",
-      title: "Netflix",
-      text: "500+ jobs",
-    },
-    {
-      img: "https://photos.wellfound.com/startups/i/32716-259c225423cff2347839275abe3b8341-medium_jpg.jpg?buster=1671766489",
-      title: "Okta",
-      text: "1000+ jobs",
-    },
-    {
-      img: "https://photos.wellfound.com/startups/i/474471-3e6bab77c052e6b5bade35ffb418eace-medium_jpg.jpg?buster=1672287611",
-      title: "BlueVine",
-      text: "600+ jobs",
-    },
-    {
-      img: "https://photos.wellfound.com/startups/i/943259-074dbb6a7ea691170f361aeee379dd57-medium_jpg.jpg?buster=1589652796",
-      title: "Open AI",
-      text: "200+ jobs",
-    },
-    {
-      img: "https://photos.wellfound.com/startups/i/37232-b86e6247bcd9846c18c91fe65ca0355d-medium_jpg.jpg?buster=1610581275",
-      title: "Stripe",
-      text: "350+ jobs",
-    },
-    {
-      img: "https://photos.wellfound.com/startups/i/25954-936e7b3ab0b5362d963486a6a96fe6c6-medium_jpg.jpg?buster=1598569121",
-      title: "Matterport",
-      text: "700+ jobs",
-    },
-  ]
+  {
+    img: "https://photos.wellfound.com/startups/i/33986-687620e87450ee4654c88a0417d81358-medium_jpg.jpg?buster=1589601889",
+    title: "Netflix",
+    text: "500+ jobs",
+  },
+  {
+    img: "https://photos.wellfound.com/startups/i/32716-259c225423cff2347839275abe3b8341-medium_jpg.jpg?buster=1671766489",
+    title: "Okta",
+    text: "1000+ jobs",
+  },
+  {
+    img: "https://photos.wellfound.com/startups/i/474471-3e6bab77c052e6b5bade35ffb418eace-medium_jpg.jpg?buster=1672287611",
+    title: "BlueVine",
+    text: "600+ jobs",
+  },
+  {
+    img: "https://photos.wellfound.com/startups/i/943259-074dbb6a7ea691170f361aeee379dd57-medium_jpg.jpg?buster=1589652796",
+    title: "Open AI",
+    text: "200+ jobs",
+  },
+  {
+    img: "https://photos.wellfound.com/startups/i/37232-b86e6247bcd9846c18c91fe65ca0355d-medium_jpg.jpg?buster=1610581275",
+    title: "Stripe",
+    text: "350+ jobs",
+  },
+  {
+    img: "https://photos.wellfound.com/startups/i/25954-936e7b3ab0b5362d963486a6a96fe6c6-medium_jpg.jpg?buster=1598569121",
+    title: "Matterport",
+    text: "700+ jobs",
+  },
+];
+
+export const HOME_NAVIGATION_OPTIONS = [
+  "Home",
+  "Trending Companies",
+  "Jobs",
+  "Services",
+];
+
+export const FREELANCER_NAVIGATION_OPTIONS = [
+  "Home",
+  "Profile",
+  "Jobs",
+  "Messages",
+  "Applied",
+];
